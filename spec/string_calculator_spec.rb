b13eb1e624713d772e8215598a57f793b3539c93
Tejaswini <tejaswini26patil@gmail.com>
2024-10-16 17:40:07 +0530

    First commit with the basec test case of empty numbers

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,13 @@
+require './string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    context 'when input is an empty string' do
+      it 'returns 0' do
+        expect(calculator.add("")).to eq(0)
+      end
+    end
+  end
+end
---
+++ string_calculator.rb
@@ -0,0 +1,9 @@
+class StringCalculator
+  def add(numbers)
+    return 0 if numbers.empty?
+  end
+end
+
+
+calc = StringCalculator.new
+puts calc.add()
